11ad76e170fe3df56b4bd5b80fbf22baf8eb75c8
Mohammed Madni <mohammedmadni09@gmail.com>
2025-03-16 20:41:29 +0530

    feat: setup basic routing

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import PlayerSelection from "./screens/PlayerSelection";
+import GameBoard from "./screens/GameBoard";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="PlayerSelection">
+        <Stack.Screen
+          name="PlayerSelection"
+          component={PlayerSelection}
+          options={{ title: "Select Players" }}
+        />
+        <Stack.Screen
+          name="GameBoard"
+          component={GameBoard}
+          options={{ title: "Snake & Ladder" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
